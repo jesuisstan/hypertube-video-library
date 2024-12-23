d6dd0dc28a4993176c7ed502e645b70c4e6d5d11
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-12-23 12:36:56 +0100

    HOST+PORT added to github route

--- src/app/api/auth/github/github-login/route.ts
+++ src/app/api/auth/github/github-login/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from 'next/server';
 
 const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
+const HOST = process.env.NEXT_PUBLIC_HOSTNAME;
+const PORT = process.env.NEXT_PUBLIC_PORT;
 
 export async function GET() {
-  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user:email&redirect_uri=http://localhost:4242/api/auth/github/github-callback`;
+  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user:email&redirect_uri=http://${HOST}:${PORT}/api/auth/github/github-callback`;
   return NextResponse.redirect(githubAuthUrl);
 }
