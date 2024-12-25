519e5c0ef623f02d2e9fb1cf7eceb7bb4ba02600
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2024-12-25 03:57:26 +0100

    loader update

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,12 +1,12 @@
 import clsx from 'clsx';
-import { SquarePlay } from 'lucide-react';
+import { LoaderCircle } from 'lucide-react';
 
 const Spinner = ({ size = 16, color }: { size?: number; color?: string }) => {
   return (
     <div>
-      <SquarePlay
+      <LoaderCircle
         size={size}
-        className={clsx('animate-ping', color ? `text-${color}` : `text-c42orange`)}
+        className={clsx('animate-spin', color ? `text-${color}` : `text-c42orange`)}
       />
     </div>
   );
