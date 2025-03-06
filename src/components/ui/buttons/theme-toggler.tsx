1309835c5b9850ba5a12a5e74fc5b3e3b55fa178
Stanislav Krivtsov <s.krivtsov@quant-cube.com>
2025-03-06 11:40:50 +0100

    style(theme): view transition api added (smooth theme toggle)

--- src/components/ui/buttons/theme-toggler.tsx
+++ src/components/ui/buttons/theme-toggler.tsx
@@ -10,12 +10,33 @@ import { ButtonCustom } from '@/components/ui/buttons/button-custom';
 const ThemeToggler = ({ translate }: { translate: (key: string) => string }) => {
   const { theme, setTheme } = useTheme();
 
+  const SWITCH = () => {
+    switch (theme) {
+      case 'light':
+        setTheme('dark');
+        break;
+      case 'dark':
+        setTheme('light');
+        break;
+      default:
+        break;
+    }
+  };
+
+  const toggleTheme = () => {
+    //@ts-ignore
+    if (!document.startViewTransition) SWITCH();
+
+    //@ts-ignore
+    document.startViewTransition(SWITCH);
+  };
+
   return (
     <ButtonCustom
       variant="ghost"
       size="icon"
       title={translate(`theme-toggle`)}
-      onClick={() => (theme === 'light' ? setTheme('dark') : setTheme('light'))}
+      onClick={toggleTheme}
       className="smooth42transition hover:bg-transparent hover:text-c42orange"
     >
       <div className="flex flex-row items-center gap-2">
