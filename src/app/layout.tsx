0a5411ac8743c529585a29e420bc4cd78e618885
George Tevosov <gtevosov@gmail.com>
2025-06-15 17:37:56 +0300

    removed unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { Exo_2 as GFont } from 'next/font/google';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 
-import { Analytics } from '@vercel/analytics/next';
 import clsx from 'clsx';
 
 import '@/styles/globals.css';
